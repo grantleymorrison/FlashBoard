f5f53ec60cb05356459cf6229e6e7327ae06a90d
Han Jung <hsjungcr@gmail.com>
2017-12-15 11:50:21 -0500

    ngApp added

---
+++ ngApp/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FlashBoard';
+}
