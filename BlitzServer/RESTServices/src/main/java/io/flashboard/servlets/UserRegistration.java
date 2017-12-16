1d535c9dd98b32577e711a15a63a8c095597325b
Michael Tseng <michael-tseng@outlook.com>
2017-12-16 18:23:28 -0500

    Integrated simple RESTful service for registration

---
+++ BlitzServer/RESTServices/src/main/java/io/flashboard/servlets/UserRegistration.java
@@ -0,0 +1,24 @@
+package io.flashboard.servlets;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/registration")
+public class UserRegistration {
+	
+	@Context
+	UriInfo uriInfo;
+	@Context
+	Request request;
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void outputData(String JSON) {
+		System.out.println(JSON);
+	}
+}
