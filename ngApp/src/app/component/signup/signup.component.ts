fe090b4dbaa0931a1acb8beff6a5c8fd3e5ff0fc
Han Jung <hsjungcr@gmail.com>
2017-12-15 22:09:45 -0500

    registration box created

--- ngApp/src/app/component/signup/signup.component.ts
+++ ngApp/src/app/component/signup/signup.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
+    styleUrls:[
+        './signup.component.css'
+    ]
   
 })
-export class SignUpComponent{}
+export class SignUpComponent{
+    constructor(){
+        
+    }
+}
