b79ebe89b44cc70cd1084f57ace271b051b0bc70
Grantley Morrison <grantley.morrison@gmail.com>
2017-12-22 13:44:20 -0500

    Update Driver.java

--- BlitzServer/RESTServices/src/main/java/io/flashboard/main/Driver.java
+++ BlitzServer/RESTServices/src/main/java/io/flashboard/main/Driver.java
@@ -1,30 +1,15 @@
 package io.flashboard.main;
 
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import io.flashboard.beans.ComprehensionTest;
-import io.flashboard.beans.Message;
-import io.flashboard.beans.TestQuestion;
 import io.flashboard.beans.User;
-import io.flashboard.util.HibernateUtil;
+import io.flashboard.dao.MessageDaoImpl;
 import io.flashboard.dao.UserDaoImpl;
 
 public class Driver {
 	public static void main(String[] args) {
-		
+		/*
 		Session session = HibernateUtil.getSession();
 		Transaction tx = null;
-		try {
-			tx = session.beginTransaction();			
-			User testUser = new User("Wilford", "Wilson","wilford", "wilfordson@gmail.com", "passwordw"); 
-			session.save(testUser); 	
-			tx.commit(); 
-			
-			tx = session.beginTransaction();
-			Message testMessage = new Message(testUser.getUserId(), "This is a comment message");
-			session.save(testMessage);
-			tx.commit(); 
-			
+		try {			
 			tx = session.beginTransaction();
 			TestQuestion tq = new TestQuestion("Math", "Pick the number with the largest prime integer factor:", "15",
 					"4", "9", "12", "15's factors are 3 and 5, both of which are primes. 12 has 6 as a factor, but 6 is not prime.", 5); 
@@ -48,9 +33,7 @@ public class Driver {
 			System.out.println(ct2);
 			tx.commit();
 			
-			tx = session.beginTransaction();
-			
-			
+			tx = session.beginTransaction();			
 		}
 		catch (Exception e) {
 			tx.rollback();
@@ -59,8 +42,14 @@ public class Driver {
 		finally {
 			session.close();
 		}
+		*/
 		
 		UserDaoImpl udi = new UserDaoImpl();
 		udi.createNewUser("James", "Jones", "jamesjones", "james.jones@mail.com", "thequickbrownfox");
+		
+		User testUser = udi.selectUserByUsername("jamesjones");
+		
+		MessageDaoImpl mdi = new MessageDaoImpl();
+		mdi.createMessage(testUser.getUserId(), "This is a comment message");
 	}
 }
