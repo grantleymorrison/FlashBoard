a0b070ccdc446162228f5a2a1e475b3792911fb2
Stuart Gillette <sjgillet@mtu.edu>
2017-12-29 10:56:58 -0500

    Added more input fields for each question on the writer's menu. Now includes topic, question text, points awarded for the question, possible answers, radio buttons to determine the correct answer, and an optional explanation for the answer to the question. Also included a small text field to clarify which answer is selected to be the correct answer. Current behavior only updates the actual answer text when the radio button is selected, or when the question would be packaged into a question model object.

--- ngApp/src/app/component/create/newquestion/newquestion.ts
+++ ngApp/src/app/component/create/newquestion/newquestion.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Question } from '../../../model/question'; 
 
 @Component ({
     selector: 'new-question',
@@ -11,11 +11,37 @@ export class NewQuestionComponent {
     @Input() index: number; 
     @Input() topic: string;
     @Input() isFromTest: boolean = false; 
-
+    
+    points:number;
+    
     option1:string;
     option2:string;
     option3:string;
     option4:string;
+
+    answerId:number = 0;
+    answerString:string;
+
+    explanation:string = ""; 
+
+    chooseAnswer(chosen):void {
+        this.answerId = chosen;
+        switch(this.answerId){
+            case 1: this.answerString = this.option1; break; 
+            case 2: this.answerString = this.option2; break; 
+            case 3: this.answerString = this.option3; break; 
+            case 4: this.answerString = this.option4; break; 
+        }
+    }
+
+    packageQuestion() {
+        switch(this.answerId){
+            case 1: this.answerString = this.option1; break; 
+            case 2: this.answerString = this.option2; break; 
+            case 3: this.answerString = this.option3; break; 
+            case 4: this.answerString = this.option4; break; 
+        }
+    }
     
     
 }
